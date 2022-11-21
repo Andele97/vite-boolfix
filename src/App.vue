ef6a41db0547276683a293f555a9f98d0eb50c69
Andele97 <andreapinna40@live.it>
2022-11-21 14:31:55 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script>
+import axios from 'axios';
+import { store } from './data/store';
+
+import AppHeader from './components/AppHeader.vue'
+import AppMain from './components/AppMain.vue'
+
+export default {
+  name: 'App',
+  data(){
+    return{
+      store
+    }
+  },
+  components: {
+    AppHeader,
+    AppMain
+  }
+}
+</script>
+
+<template>
+  <AppHeadear />
+  <AppMain />
+</template>
+
+<style lang="scss">
+
+ @use './styles/general.scss'
+
+</style>
